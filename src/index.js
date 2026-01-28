a906c722557d9e4945ca43215d417a6beba21d4f
shikhrshukla <shikhar31690.1@gmail.com>
2026-01-29 00:29:32 +0530

    util files added

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@
 // require("dotenv").config({path: "./.env"});
 import connectDB from "./db/connection.js";
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.on("error", (error)=> {
+        console.error("Express app error: ", error.message);
+        throw error;
+    })
+    app.listen(process.env.PORT || 4000, ()=> {
+        console.log(`Servier is running at PORT ${process.env.PORT}`);
+    })
+})
+.catch((error)=> {
+    console.error("MongoDB connection failed: ", error.message);
+})
