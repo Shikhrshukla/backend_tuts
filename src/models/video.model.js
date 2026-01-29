cf6401cc2563d32a98899f8fce2ffb19d3a86f45
shikhrshukla <shikhar31690.1@gmail.com>
2026-01-29 22:05:42 +0530

    add user and video models

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        titile: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    }, { timestamps: true });
+
+videoSchema
+
+export const videoModel = mongoose.model("Video", videoSchema);
