39b3d2d8b04d6dd22f9e58c015b6c70e4aec6aef
shikhrshukla <shikhar31690.1@gmail.com>
2026-01-31 23:35:31 +0530

    made a basic user registration endpoint

--- src/app.js
+++ src/app.js
@@ -7,16 +7,17 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}))  
 
 app.use(express.json({ limit: "16kb"}));
-app.use(urlencoded(
-    {
-        extended: true,
-        limit: "16kb"
-    }
-))
+app.use(urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+// Routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@
 
 // require("dotenv").config({path: "./.env"});
 import connectDB from "./db/connection.js";
-import express from "express";
-
-const app = express();
+import { app } from "./app.js";
 
 connectDB()
-.then(()=> {
-    app.on("error", (error)=> {
-        console.error("Express app error: ", error.message);
-        throw error;
+    .then(() => {
+        app.on("error", (error) => {
+            console.error("Express app error: ", error.message);
+            throw error;
+        })
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is running at PORT ${process.env.PORT}`);
+        })
     })
-    app.listen(process.env.PORT || 4000, ()=> {
-        console.log(`Server is running at PORT ${process.env.PORT}`);
+    .catch((error) => {
+        console.error("MongoDB connection failed: ", error.message);
     })
-})
-.catch((error)=> {
-    console.error("MongoDB connection failed: ", error.message);
-})
